108f2268551d9dd6ce75412617f1f3fba06ff44c
SaraBarrett <sara.c.mon@gmail.com>
2025-06-04 13:30:32 +0100

    dom

---
+++ js/exs.js
@@ -0,0 +1,19 @@
+// let img = document.getElementById('unicorn');
+// let myH1= document.getElementById('mainheading');
+// if(img){
+//     console.log(img)
+// }
+
+// console.log(myH1)
+
+
+
+// let doneTodos = document.querySelectorAll('.done');
+// const checkBox = document.querySelector("input[type='checkbox']")
+// console.log(doneTodos)
+// console.log(checkBox)'
+
+function changeTaste(){
+    let mySpan = document.querySelector('span');
+    mySpan.innerText = 'Yack'
+}
---
+++ js/script.js
@@ -0,0 +1,10 @@
+const mySpan = document.getElementById("History");
+console.log(mySpan);
+
+mySpan.innerText = "História";
+
+// const myDivs = document.getElementsByTagName('div');
+// console.log(myDivs)
+
+// const myDivsQ = document.querySelectorAll('div');
+// console.log(myDivsQ)
