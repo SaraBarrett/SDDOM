332e568698d8fee283b65ed0278e1f3bae57848c
SaraBarrett <sara.c.mon@gmail.com>
2025-06-05 10:40:52 +0100

    style

--- js/exs.js
+++ js/exs.js
@@ -17,3 +17,39 @@ function changeTaste(){
     let mySpan = document.querySelector('span');
     mySpan.innerText = 'Yack'
 }
+
+function eggOrChicken(){
+    let myImg = document.querySelector('img');
+
+   if(myImg.getAttribute('src') =='https://devsprouthosting.com/images/chicken.jpg'){
+        myImg.setAttribute('src', 'https://devsprouthosting.com/images/egg.jpg')
+   }else{
+        myImg.setAttribute('src', 'https://devsprouthosting.com/images/chicken.jpg');
+   } 
+
+
+
+}
+
+function changeLayout(){
+     let myDiv = document.getElementById('container');
+     myDiv.style.textAlign = 'center';
+     
+     let myImg = document.querySelector('img');
+     myImg.style.width = '150px';
+     myImg.style.borderRadius = '50%'
+}
+
+
+let spans = document.querySelectorAll('span');
+const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+
+let counter = 0;
+for(let element of spans){
+    element.style.color = colors[counter]
+     counter++
+}
+
+// spans.forEach((element, index) => {
+//      element.style.color = colors[index] 
+// })
--- js/script.js
+++ js/script.js
@@ -8,3 +8,18 @@ mySpan.innerText = "História";
 
 // const myDivsQ = document.querySelectorAll('div');
 // console.log(myDivsQ)
+
+
+let myLis = document.querySelectorAll('.toclevel-1');
+
+for(let li of myLis){
+    //adiciona
+    li.classList.add('classListTest')
+
+    //remove
+    li.classList.remove('toclevel-1')
+
+    //verificar se tem
+    console.log(li.classList.contains('toclevel-1'))
+
+}
